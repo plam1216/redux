0d32bfc58272e50aa89bd0dd7a665fec8a5d7fbf
Peter Lam <plam1216@gmail.com>
2023-03-08 22:43:32 -0500

    counter using Provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./actions";
+
+
 function App() {
+  // counter data is received from <Provider> in index.js
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Counter {counter}</h1>
+
+      <button
+        onClick={() => dispatch(increment(5))}
+      >
+        +
+      </button>
+      
+      <button
+        onClick={() => dispatch(decrement(5))}
+      >
+        -
+      </button>
+      
+      
+      {isLogged ? <h3>Logged In</h3> : <h3>Not Logged</h3>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,49 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import allReducers from './reducers';
 
 
 // STORE -> GLOBALIZED STATE
-
+const store = configureStore({
+  reducer: allReducers,
+  devTools: true
+})
 
 // ACTION -> "INCREMENT"
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
 
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
 
 // REDUCER -> TRANSFORM STATE BASED ON ACTION
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
-let store = configureStore({reducer: counter})
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//   }
+// }
 
 // Display to console
-store.subscribe(() => console.log(store.getState()))
+
+// store.subscribe(() => console.log(store.getState()))
 
 
 // DISPATCH -> SEND THE ACTION TO REDUCER
-store.dispatch(increment())
-store.dispatch(decrement())
-store.dispatch(decrement())
+// store.dispatch(increment())
+// store.dispatch(decrement())
+// store.dispatch(decrement())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
